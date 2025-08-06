80e2797dccdf71aa94460a46384d317a61370aaa
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-06 16:37:07 +0200

    Update weather parameters and enhance rain prediction logic in main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ api_key = "0983b94d454a0bfa8a982b1f3b1bceae"
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 
 weather_params = {
-    "lat": 49.414169,
-    "lon": 14.658740,
+    "lat": 17.246373,
+    "lon": -10.672252,
     "cnt": 4,
     "appid": api_key,
 }
@@ -20,8 +20,19 @@ response.raise_for_status()
 weather_data = response.json()
 
 
-print(weather_data)
+print(weather_data["list"][0]["weather"])
 
 
-print(dt.datetime.fromtimestamp(1754449200))
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    time = dt.datetime.fromtimestamp(hour_data["dt"])
+    condition_code = int(hour_data["weather"][0]["id"])
+
+    if condition_code < 700:
+        will_rain = True
+    print(f"{time.hour}:00", condition_code)
+
+if will_rain is True:
+    print("Bring the umbrella.")
 
